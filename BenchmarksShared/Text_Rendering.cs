08bc418ff537cd032a37169179f2c8407f3cf5d0
Steven Kirk <grokys@users.noreply.github.com>
2024-02-14 17:36:51 +0100

    Added GlyphRun rendering benchmark.

--- BenchmarksShared/Text_Rendering.cs
+++ BenchmarksShared/Text_Rendering.cs
@@ -14,9 +14,10 @@ namespace AvaloniaBenchmarks
         private readonly SKCanvas _skCanvas = null!;
         private readonly SKPaint _paint = new() { Color = new(0, 0, 0) };
         private readonly DrawingContext _drawingContext = null!;
-        private readonly Typeface typeface = new("Arial");
+        private readonly Typeface _typeface = new("Arial");
         private readonly TextLayout _shortText;
         private readonly FormattedText _shortFormattedText;
+        private readonly GlyphRun _shortGlyphRun;
 
         public Text_Rendering()
         {
@@ -26,12 +27,26 @@ namespace AvaloniaBenchmarks
             _drawingContext = _target.CreateDrawingContext(false);
 #endif
             _drawingContext.DrawRectangle(Brushes.White, null, new Rect(0, 0, 1000, 1000));
-            _shortText = new TextLayout("text", typeface, 48, Brushes.Black);
+            _shortText = new TextLayout("text", _typeface, 48, Brushes.Black);
 #if AVALONIA10
-            _shortFormattedText = new FormattedText { Text = "text", Typeface = typeface, FontSize = 48 };
+            _shortFormattedText = new FormattedText { Text = "text", Typeface = _typeface, FontSize = 48 };
 #else
-            _shortFormattedText = new FormattedText("text", CultureInfo.InvariantCulture, FlowDirection.LeftToRight, typeface, 48, Brushes.Black);
+            _shortFormattedText = new FormattedText("text", CultureInfo.InvariantCulture, FlowDirection.LeftToRight, _typeface, 48, Brushes.Black);
 #endif
+
+#if !AVALONIA10
+            _shortGlyphRun = new GlyphRun(
+                _typeface.GlyphTypeface,
+                48,
+                "text".AsMemory(),
+                [
+                    _typeface.GlyphTypeface.GetGlyph('t'),
+                    _typeface.GlyphTypeface.GetGlyph('e'),
+                    _typeface.GlyphTypeface.GetGlyph('x'),
+                    _typeface.GlyphTypeface.GetGlyph('t'),
+                ]);
+#endif
+
             _skCanvas = new SKCanvas(new SKBitmap(1000, 1000));
         }
 
@@ -63,6 +78,21 @@ namespace AvaloniaBenchmarks
                 }
             }
         }
+
+#if !AVALONIA10
+        [Benchmark]
+        public void Render_Short_GlyphRun()
+        {
+            for (var y = 0; y < 10; y++)
+            {
+                for (var x = 0; x < 10; x++)
+                {
+                    using var p = _drawingContext.PushTransform(Matrix.CreateTranslation(x * 10, y * 10));
+                    _drawingContext.DrawGlyphRun(Brushes.Black, _shortGlyphRun);
+                }
+            }
+        }
+#endif
 
         [Benchmark]
         public void Render_Short_FormattedText()
